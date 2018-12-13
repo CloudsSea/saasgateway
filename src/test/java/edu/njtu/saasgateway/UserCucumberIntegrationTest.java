8983bf44aeaa24d93bda18dd6685cb7297ac5c58
CloudsSeas <374737719@qq.com>
2018-12-13 15:18:10 +0800

    bug 修改

--- src/test/java/edu/njtu/saasgateway/UserCucumberIntegrationTest.java
+++ src/test/java/edu/njtu/saasgateway/UserCucumberIntegrationTest.java
@@ -6,5 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/resources/features/user.feature", plugin = {"pretty", "html:target/cucumber"})
 public class UserCucumberIntegrationTest {
-
+	public static void main(String[] args) {
+		
+	}
 }
